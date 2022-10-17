12a7cc30a4d8b1f584d8b499c1e20c2f809ab340
jaewooMaeng <maeng9584@likelion.org>
2022-10-17 21:56:02 +0900

    unittest

--- toh/hero/tests.py
+++ toh/hero/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from pydoc import cli
+from django.test import TestCase, Client
+from .models import Hero
 
-# Create your tests here.
+class HeroTestCase(TestCase):
+    def setUp(self) -> None:
+        Hero.objects.create(name='Superman')
+        Hero.objects.create(name='Batman')
+        Hero.objects.create(name='Ironman')
+
+    def test_hero_count(self):
+        self.assertEqual(Hero.objects.all().count(), 3)
+
+    def test_hero_id(self) -> None:
+        client = Client()
+        response = client.get('/hero/1/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('1', response.content.decode())
+
